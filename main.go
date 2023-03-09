8c1ac1b4bd7a0ee5fd2fdf1dd23326b7ab368b0e
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-03-09 12:04:41 +0100

    DNS

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+var domain string = "google.com"
+
+func main() {
+	// Resolve the domain name using the default DNS resolver
+	ips, err := net.LookupIP(domain)
+	if err != nil {
+		fmt.Println("Error resolving domain name:", err)
+		return
+	}
+
+	// Print the IP addresses associated with the domain name
+	for _, ip := range ips {
+		fmt.Println(domain, "->", ip)
+	}
+}
